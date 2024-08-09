6615b23daf595cd2baafcfdce93bd5841893c328
Abdullah-T3 <eng.abdullahahmed59@gmail.com>
2024-08-09 17:27:27 +0300

    .

--- lib/componentes/Button.dart
+++ lib/componentes/Button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CustButton extends StatelessWidget {
-
   final String text;
   final String? logo;
   final Function()? onPressed;
@@ -13,26 +12,25 @@ class CustButton extends StatelessWidget {
       onPressed: onPressed,
       child: logo != null
           ? Container(
-        child: Row(
-          children: [
-            Container(
-              height: 20,
-              width: 20,
-              child: Image.asset(logo!),
-            ),
-            SizedBox(width: 10),
-            Text(
-              text,
-            ),
-          ],
-        ),
-      )
+              child: Row(
+                children: [
+                  Container(
+                    height: 20,
+                    width: 20,
+                    child: Image.asset(logo!),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    text,
+                  ),
+                ],
+              ),
+            )
           : Text(
-        text,
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-      ),
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
       materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
     );
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_nots/views/HomePage_View.dart';
 import 'package:my_nots/views/SignInPage_View.dart';
+import 'package:my_nots/views/SignUpPage_View.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +15,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
+      routes: {
+        "/login": (context) => LoginPage(),
+        "/signup": (context) => SignupPage(),
+        "/home": (context) => Homepage(),
+      },
     );
     throw UnimplementedError();
   }}
